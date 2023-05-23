27e951260601f933f9075bb76e0dc60853520287
Documenter.jl <documenter@juliadocs.github.io>
2023-05-23 17:19:00 +0000

    build based on 0980cb0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"#PetroBase.jl","page":"Home","title":"PetroBase.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is a basic package with some useful type definitions and functions for petrological modelling. This is intended for use with JPerpleX and other modelling packages I have developed to provide an easy way to interoperate between them.","category":"page"}]
 }
