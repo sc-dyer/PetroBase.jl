7dae724a2894f2d94b8396676689a81fe11f1204
sc-dyer <36042586+sc-dyer@users.noreply.github.com>
2023-05-23 13:05:40 -0400

    Added junk for Documenter
    Hoping this will work and make documentation happen

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,25 @@
-using 
-    Documenter,  
-    PetroBase
-# push!(LOAD_PATH,"../src/")
-makedocs(sitename="PetroBase.jl")
-deploydocs(repo = "github.com/sc-dyer/PetroBase.jl.git")
+using PetroBase
+using Documenter
+
+DocMeta.setdocmeta!(PetroBase, :DocTestSetup, :(using PetroBase); recursive=true)
+
+makedocs(;
+    modules=[PetroBase],
+    authors="Sabastien Dyer <scdyer@uwaterloo.ca> and contributors",
+    repo="https://github.com/sc-dyer/PetroBase.jl/blob/{commit}{path}#{line}",
+    sitename="PetroBase.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://sc-dyer.github.io/PetroBase.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/sc-dyer/PetroBase.jl",
+    devbranch="main",
+)
