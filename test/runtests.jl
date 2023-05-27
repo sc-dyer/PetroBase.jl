43e986895d71a3341d667d8d33b40f8046fd91b1
sc-dyer <36042586+sc-dyer@users.noreply.github.com>
2023-05-27 11:30:50 -0400

    Added tests for complete coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ using Test
     #Same with trace elements
     te1 = TraceElem("Y",88.9059,400.0)
     te2 = TraceElem(te1,900.0)
+    te3 = TraceElem("Lu", 174.967, 50.0)
 
     #Testing the operators and simple method calls
     @test name(comp2) == "SiO2"
@@ -23,19 +24,25 @@ using Test
     @test conc(comp3) ≈ 1
     @test conc(te1) ≈ 400
     @test conc(comp1 + comp2) ≈ 4
+    @test conc(1 + comp2) ≈ 4
     @test conc(te1+te2) ≈ 1300
     @test conc(comp2 - comp1) ≈ 2
+    @test conc(comp2 - 1) ≈ 2
     @test conc(te2 - te1) ≈ 500
+    @test conc(te2 - 400) ≈ 500
     @test conc(10*comp1) ≈ 10
     @test conc(comp1/10) ≈ 0.1
     @test conc(10*te1) ≈ 4000
     @test conc(te1/10) ≈ 40
     @test_throws ArgumentError comp1 + comp4
+    @test_throws ArgumentError te1 + te3
+    @test_throws ArgumentError comp4 - comp1
+    @test_throws ArgumentError te3-te1
 
     
     compoList1 = [comp1,comp4]
     compoList2 = [comp1,comp2,comp3]
-    te3 = TraceElem("Lu", 174.967, 50.0)
+   
     teList1 = [te1,te3]
     teList2 = [te1,te2,te3]
     #Now testing some more complicated functions
@@ -46,6 +53,8 @@ using Test
     @test !checkUnique(teList2)
     @test findChem(compoList1,comp4) == 2
     @test findChem(compoList1,"MgO") == 2
+    @test findChem(compoList1,Component("K2O",94.2,3.0,0.0)) == 0
+    @test findChem(compoList1, "K2O") == 0
     @test findChem(teList1,te3) == 2
     @test findChem(teList1,"Lu") ==2
 
